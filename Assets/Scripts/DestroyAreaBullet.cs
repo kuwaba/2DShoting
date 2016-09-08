27bd459b13a2287bb148213827a6b76b71be7fc3
moigoo <moigonn@gmail.com>
2016-09-09 02:36:33 +0900

    asset commit

---
+++ Assets/Scripts/DestroyAreaBullet.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAreaBullet : MonoBehaviour {
+
+    void Start()
+    {
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        Vector2 size = max * 3;
+
+        GetComponent<BoxCollider2D>().size = size;
+    }
+
+
+    void OnTriggerExit2D(Collider2D c)
+    {
+        string layerName = LayerMask.LayerToName(c.gameObject.layer);
+
+        if (layerName != ("Bullet(Player)") && (layerName != ("Bullet(Enemy)")))
+        {
+
+            Destroy(c.gameObject);
+        }
+    }
+}
