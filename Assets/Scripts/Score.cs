4c571c14e8c93c685a67aecd17ef759b50f122be
kuwabata <kuwabata@mbi.cs.tut.ac.jp>
2016-09-08 22:00:40 +0900

    asset cmit

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+    public GUIText scoreGUIText;
+    public GUIText highScoreGUIText;
+
+    private int score;
+
+    private int highScore;
+
+    private string highScoreKey = "highScore";
+
+
+    // Use this for initialization
+    void Start () {
+        Initialize();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(highScore < score)
+        {
+            highScore = score;
+        }
+        scoreGUIText.text = "Score:"  + score.ToString();
+        highScoreGUIText.text = "HighScore:" +  highScore.ToString();
+	
+	}
+
+    private void Initialize()
+    {
+        score = 0;
+
+        highScore = PlayerPrefs.GetInt(highScoreKey, 0);
+    }
+
+    public void AddPoint(int point)
+    {
+        score = score + point;
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetInt(highScoreKey, highScore);
+        PlayerPrefs.Save();
+
+        Initialize();
+    }
+}
