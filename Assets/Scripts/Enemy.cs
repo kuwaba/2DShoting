5a65e533f242a09484a9de0349dbb364ffe796ab
moigoo <moigonn@gmail.com>
2016-09-08 10:54:44 +0900

    firstcommmt

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+
+    Spaceship spaceship;
+    // Use this for initialization
+	IEnumerator Start () {
+        spaceship = GetComponent<Spaceship>();
+
+        Move(transform.up * -1);
+        if (!spaceship.canShot)
+        {
+            yield break;
+        }
+
+
+        while (true)
+        {
+            for(int i = 0; i < transform.childCount; i++)
+            {
+                Transform shotPosition = transform.GetChild(i);
+
+                spaceship.Shot(shotPosition);
+            }
+            yield return new WaitForSeconds(spaceship.shotDelay);
+        }
+        
+    }
+    public void Move(Vector2 direction)
+    {
+        GetComponent<Rigidbody2D>().velocity = direction * spaceship.speed;
+    }
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        string layerName = LayerMask.LayerToName(c.gameObject.layer);
+
+        if (layerName == ("Bullet(Player)"))
+        {
+
+            Destroy(c.gameObject);
+        }
+        if (layerName == ("Bullet(Player)") || layerName == ("Player"))
+        {
+
+            spaceship.Explosion();
+
+            Destroy(gameObject);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Spaceship.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Spaceship : MonoBehaviour {
+
+    public float speed;
+
+    public float shotDelay;
+
+    public GameObject bullet;
+
+    public GameObject explosion;
+
+    public bool canShot;
+	// Use this for initialization
+	public void Shot (Transform origin){
+        Instantiate(bullet, origin.position, origin.rotation);
+	
+	}
+	
+   
+    public void Explosion()
+    {
+        Instantiate(explosion, transform.position, transform.rotation);
+    }
+    
+}
